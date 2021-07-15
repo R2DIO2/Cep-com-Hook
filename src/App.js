3954ed28eaa646068528297c58f57e674bc91ba1
Diogenes de Oliveira <diogenesoliveiraa@gmail.com>
2021-07-14 21:48:08 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [cep, setCep] = useState({});
+
+  function handleTextChange(e) {
+    e.preventDefault();
+    fetch(`https://viacep.com.br/ws/${e.target.value}/json/`).then(res => res.json())
+      .then(res => setCep(res))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <strong>Olá, {props.name}</strong>
+      <div>
+        Digite o cep: <input type="text" onChange={handleTextChange}></input>
+        {Object.entries(cep).map((values, key) => (
+          <h2 key={key}>
+            <strong>{values[0]}:</strong>{values[1]}
+          </h2>
+        ))}
+      </div>
     </div>
   );
 }
